695f97d724cfa37a57df1db7b364740a14d76c75
Ananya <ananyaanu3125@gmail.com>
2025-09-16 10:22:32 +0530

    Update file

--- lab 1 - data operations using pointers/swapUsingPointers.c
+++ lab 1 - data operations using pointers/swapUsingPointers.c
@@ -4,8 +4,8 @@ int main() {
 	printf("Before swapping: a = %d b = %d\n",a,b);
 	int *p = &a, *q = &b;
 	int temp = *p;
-	a = *q;
-	b = temp;
+	*p = *q;
+	*q = temp;
 	printf("After swapping: a = %d b = %d\n",a,b);
 	return 0;
 }
